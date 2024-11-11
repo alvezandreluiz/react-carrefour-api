a3a4785bc59597bba68adad666cc0b543cc0514a
alvezandreluiz <alvezandreluiz@gmail.com>
2024-11-11 16:29:46 -0300

    Ajustes no carrinho de compras

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
+  const [cartCount, setCartCount] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (product) => {
+    setCartCount(prevCount => prevCount + 1);
+    setCartItems(prevItems => [...prevItems, product]);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Content />
+      <Header cartCount={cartCount} cartItems={cartItems} />
+      <Content addToCart={addToCart} />
       <Footer />
     </div>
   );
